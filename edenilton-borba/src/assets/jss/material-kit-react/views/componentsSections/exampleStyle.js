b1c2ebce34ca9360af51590eae9ecbd025da2539
Edenilton <edeniltonborba.adm@gmail.com>
2020-03-19 23:28:44 -0300

    add cv

--- edenilton-borba/src/assets/jss/material-kit-react/views/componentsSections/exampleStyle.js
+++ edenilton-borba/src/assets/jss/material-kit-react/views/componentsSections/exampleStyle.js
@@ -4,35 +4,13 @@ import imagesStyle from "assets/jss/material-kit-react/imagesStyles.js";
 
 const exampleStyle = {
   section: {
-    width: "100%",
-    position: "relative",
-    flexBasis: "auto",
-    minHeight: "1px",
-    paddingLeft: "15px",
-    paddingRight: "15px",
+    padding: "20px 0",
   },
   container: {
     ...conatinerFluid,
     textAlign: "center !important",
   },
 
-  servicesStyle: {
-    color: "#555",
-    width: "100%",
-    border: "0",
-    display: "flex",
-    padding: "0.625rem 0",
-    zIndex: "unset",
-    position: "relative",
-    flexFlow: 'row nowrap',
-    boxShadow: "0 4px 18px 0px rgba(0, 0, 0, 0.12), 0 7px 10px -5px rgba(0, 0, 0, 0.15)",
-    transition: "all 150ms ease 0s",
-    alignItems: "center",
-    borderRadius: "3px",
-    marginBottom: "20px",
-    justifyContent: "flex-start",
-    backgroundColor: "#fff",
-  },
   ...imagesStyle,
   link: {
     textDecoration: "none"
--- edenilton-borba/src/views/Components/Sections/SectionServices.js
+++ edenilton-borba/src/views/Components/Sections/SectionServices.js
@@ -25,23 +25,22 @@ export default function SectionServices() {
                 <GridContainer justify="center">
                     <GridItem xs={12} sm={12} md={6}>
                         <div className={classes.typo}>
+                            <div className={classes.note}>Front End Web Development</div>
                             <p>
-                                I will be the leader of a company that ends up being worth
-                                billions of dollars, because I got the answers. I understand
-                                culture. I am the nucleus. I think that’s a responsibility that
-                                I have, to push possibilities, to show people, this is the level
-                                that things could be at.
+                               
+              Develop Front End using latest standards with HTML5/CSS3 with added funtionality using JavaScript and
+              React.
+            
                             </p>
                         </div>
                     </GridItem>
                     <GridItem xs={12} sm={12} md={6}>
                         <div className={classes.typo}>
+                            <div className={classes.note}>Back End Web Development</div>
                             <p>
-                                I will be the leader of a company that ends up being worth
-                                billions of dollars, because I got the answers. I understand
-                                culture. I am the nucleus. I think that’s a responsibility that
-                                I have, to push possibilities, to show people, this is the level
-                                that things could be at.
+                                
+              Develop Back End application/service using Node.js and SQL or NoSQL DB databases.
+            
                             </p>
                         </div>
 
