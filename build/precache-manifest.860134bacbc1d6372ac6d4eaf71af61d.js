92283e34abf58af34d1dddada445b1b241423dfe
Edenilton <edeniltonborba.adm@gmail.com>
2020-03-26 12:10:49 -0300

    update

---
+++ build/precache-manifest.860134bacbc1d6372ac6d4eaf71af61d.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "33fb7dbcf039171a0737b36e1886b512",
+    "url": "/index.html"
+  },
+  {
+    "revision": "51058a880c6b32d99202",
+    "url": "/static/css/main.7828ff43.chunk.css"
+  },
+  {
+    "revision": "42a9a7368c3966949d35",
+    "url": "/static/js/2.9834fe41.chunk.js"
+  },
+  {
+    "revision": "51058a880c6b32d99202",
+    "url": "/static/js/main.a261d5bf.chunk.js"
+  },
+  {
+    "revision": "7ef6924db03ec9753c79",
+    "url": "/static/js/runtime~main.2efed672.js"
+  },
+  {
+    "revision": "1624d3309136da293023caa54a026f33",
+    "url": "/static/media/CodeQuiz.1624d330.png"
+  },
+  {
+    "revision": "3a1124ca334c1ccf746b9bb64ac1d3ae",
+    "url": "/static/media/DayPlanner.3a1124ca.png"
+  },
+  {
+    "revision": "8ebf3446da01d938cb231ae58ca28488",
+    "url": "/static/media/Deni.8ebf3446.jpg"
+  },
+  {
+    "revision": "9613859abffe402790976dfbd12761f4",
+    "url": "/static/media/Dev Profile Generator.9613859a.png"
+  },
+  {
+    "revision": "f939dbfb25935238c224da405608675d",
+    "url": "/static/media/EmployeeTracker.f939dbfb.png"
+  },
+  {
+    "revision": "fba6996b6719dcf3cda91b97db7b67bc",
+    "url": "/static/media/FotoPerfil.fba6996b.jpg"
+  },
+  {
+    "revision": "9b0ee4cea89d5f844192f6b4a1a35b12",
+    "url": "/static/media/LastMinuteTrip.9b0ee4ce.png"
+  },
+  {
+    "revision": "c4408e289292d99c4702ecc8eadbf4ae",
+    "url": "/static/media/NodeExpress.c4408e28.png"
+  },
+  {
+    "revision": "cddc1a6dd7160705af3512748d0d1980",
+    "url": "/static/media/NoteTaker.cddc1a6d.png"
+  },
+  {
+    "revision": "b49b0dd2cf5721fecdb3e3799fa06b0f",
+    "url": "/static/media/Search API Worked.b49b0dd2.png"
+  },
+  {
+    "revision": "d93a4a2f33d56294bc47418452091ca0",
+    "url": "/static/media/Supermarket.d93a4a2f.png"
+  },
+  {
+    "revision": "9a928af720230b15575c6bbf567cec43",
+    "url": "/static/media/TemplateEngine.9a928af7.png"
+  },
+  {
+    "revision": "447873deb363121058c99a870699901d",
+    "url": "/static/media/UnderConstruction.447873de.png"
+  },
+  {
+    "revision": "4f1841f32bd24145d536827096c78cf5",
+    "url": "/static/media/WordGuessGame.4f1841f3.png"
+  },
+  {
+    "revision": "c0274b7d40ef12617693c360e32b0efe",
+    "url": "/static/media/WorkoutTracker.c0274b7d.png"
+  },
+  {
+    "revision": "843781834b1db2908f08d921c6460c81",
+    "url": "/static/media/bg2.84378183.jpg"
+  },
+  {
+    "revision": "17f12ee9039334fbcb846a61a72026fe",
+    "url": "/static/media/goodwork.17f12ee9.png"
+  },
+  {
+    "revision": "cf8b686b294041d0925f4e745b1fabb9",
+    "url": "/static/media/sign.cf8b686b.jpg"
+  }
+]);
